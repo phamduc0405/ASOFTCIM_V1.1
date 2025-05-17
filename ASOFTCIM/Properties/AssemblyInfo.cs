0e01459ab84087dfbf7fb3796d69a3698f10103f
nam pham <nampham9bnqa@gmail.com>
2025-05-17 14:34:08 +0700

    //Update ECM su dung MVVM

--- ASOFTCIM/Properties/AssemblyInfo.cs
+++ ASOFTCIM/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.1.25")]
-[assembly: AssemblyFileVersion("1.0.1.25")]
+[assembly: AssemblyVersion("1.0.1.27")]
+[assembly: AssemblyFileVersion("1.0.1.27")]
